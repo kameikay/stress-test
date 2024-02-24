5ddb586d118de5b97c99e875655293de26729285
Victor Kamei Kay <kameikay@gmail.com>
2024-02-24 15:05:02 -0300

    add repository

---
+++ internal/repository/repository.go
@@ -0,0 +1,48 @@
+package repository
+
+type RequestsMemory struct {
+	Requests ReportRequest
+}
+
+type ReportRequest map[int]int
+
+func NewRequestsMemory() *RequestsMemory {
+	return &RequestsMemory{
+		Requests: make(ReportRequest),
+	}
+}
+
+func (s *RequestsMemory) Save(statusCode int) {
+	_, ok := s.Requests[statusCode]
+	if ok {
+		s.Requests[statusCode]++
+	} else {
+		s.Requests[statusCode] = 1
+	}
+}
+
+func (s *RequestsMemory) GetTotalRequests() int {
+	total := 0
+
+	for _, v := range s.Requests {
+		total += v
+	}
+
+	return total
+}
+
+func (s *RequestsMemory) GetStatus200() int {
+	return s.Requests[200]
+}
+
+func (s *RequestsMemory) GetOthersStatus() map[int]int {
+	others := make(map[int]int)
+
+	for k, v := range s.Requests {
+		if k != 200 {
+			others[k] = v
+		}
+	}
+
+	return others
+}
---
+++ internal/repository/repository_test.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/suite"
+)
+
+type RequestsMemoryTestSuite struct {
+	suite.Suite
+	ctrl *gomock.Controller
+}
+
+func TestRequestsMemoryStart(t *testing.T) {
+	suite.Run(t, new(RequestsMemoryTestSuite))
+}
+
+func (suite *RequestsMemoryTestSuite) RequestsMemoryTestSuiteDown() {
+	defer suite.ctrl.Finish()
+}
+
+func (suite *RequestsMemoryTestSuite) SetupTest() {
+	suite.ctrl = gomock.NewController(suite.T())
+}
+
+func (suite *RequestsMemoryTestSuite) TestNewRequestsMemory() {
+	requestsMemory := NewRequestsMemory()
+	suite.NotNil(requestsMemory)
+}
+
+func (suite *RequestsMemoryTestSuite) TestSave() {
+	requestsMemory := NewRequestsMemory()
+	requestsMemory.Save(200)
+	requestsMemory.Save(200)
+	requestsMemory.Save(404)
+
+	suite.Equal(2, requestsMemory.Requests[200])
+	suite.Equal(1, requestsMemory.Requests[404])
+}
+
+func (suite *RequestsMemoryTestSuite) TestGetTotalRequests() {
+	requestsMemory := NewRequestsMemory()
+	requestsMemory.Save(200)
+	requestsMemory.Save(200)
+	requestsMemory.Save(404)
+
+	suite.Equal(3, requestsMemory.GetTotalRequests())
+}
+
+func (suite *RequestsMemoryTestSuite) TestGetStatus200() {
+	requestsMemory := NewRequestsMemory()
+	requestsMemory.Save(200)
+	requestsMemory.Save(200)
+	requestsMemory.Save(404)
+
+	suite.Equal(2, requestsMemory.GetStatus200())
+}
+
+func (suite *RequestsMemoryTestSuite) TestGetOthersStatus() {
+	requestsMemory := NewRequestsMemory()
+	requestsMemory.Save(200)
+	requestsMemory.Save(200)
+	requestsMemory.Save(404)
+
+	others := requestsMemory.GetOthersStatus()
+	suite.Equal(1, others[404])
+}
